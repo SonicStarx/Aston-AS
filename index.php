843681fdad335eecc7255c8f7c11f1f80749eabc
Lainey <37411596+SonicStarx@users.noreply.github.com>
2019-04-02 21:23:01 +0100

    Work on first page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <p> staff</p>
+</header>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link href="index.css" rel="stylesheet" type="text/css" />
+  <img src="images/aston-logo.png" class = "align-center" id = "logo">
+</head>
+<br>
+<body>
+  <center> <div class = "homeImages">
+  <img src="images/withAnimals.jpg">
+  <img src="images/bear.jpg" >
+</div>
+<br>
+<button type="button" class="btn btn-primary">Login</button>
+<button type="button" class="btn btn-primary">Register</button>
+</center>
+</body>
+</html>
